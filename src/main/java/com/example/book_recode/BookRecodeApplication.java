023d980ce4bc5c474669f4da54313eaae59c1365
victoriaum <y.ilpyo95@gmail.com>
2022-03-16 23:45:26 +0900

    Initial commit

---
+++ src/main/java/com/example/book_recode/BookRecodeApplication.java
@@ -0,0 +1,13 @@
+package com.example.book_recode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRecodeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BookRecodeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/book_recode/BookRecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.book_recode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRecodeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
